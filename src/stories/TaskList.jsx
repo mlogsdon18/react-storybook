2d25a0fbc1f20c02f5912bebcc0c2ce399a46827
Megan Logsdon <megan@heysparkbox.com>
2022-02-16 11:27:57 -0500

    :white_check_mark: Add tests for TaskList story WithPinnedTasks

--- src/stories/TaskList.jsx
+++ src/stories/TaskList.jsx
@@ -7,20 +7,65 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     onPinTask,
     onArchiveTask,
   };
+  const LoadingRow = (
+    <div className="cmp-loading">
+      <span className="cmp-loading__checkbox glow-checkbox" />
+      <span className="cmp-loading__text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
 
   if (loading) {
-    return <div className="list-items">loading</div>;
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items">empty</div>;
+    return (
+      <div className="cmp-list" key={"empty"} data-testid="empty">
+        <div className="cmp-message">
+          <span className="cmp-message__icon icon-check" />
+          <div className="cmp-message__title">You have no tasks</div>
+          <div className="cmp-message__subtitle">Sit back and relax</div>
+        </div>
+      </div>
+    );
   }
 
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+
   return (
-    <div className="list-items">
-      {tasks.map(task => (
+    <div className="cmp-list">
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
   );
 }
+
+
+TaskList.propTypes = {
+/** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+  loading: false,
+};
